1ca56fb3e2fd238fcdf3d21631ba65d22ecfc70a
kobosh <kokobosh@gmail.com>
2021-12-16 09:29:59 -0500

    added Menu class

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,7 +1,14 @@
 package com.revature;
 
+import com.revature.models.Menu;
+
+//import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Driver {
 
     public static void main(String[] args) {
+    	
+    	Menu  men=new Menu();
+    	men.displayMenu();
     }
 }
